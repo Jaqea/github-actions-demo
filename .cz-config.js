87ba303c18752ec1068fd2ebda190d1e8a3e11ea
Jaqea <1601676692@qq.com>
2022-07-31 15:19:34 +0800

    feat: github actions
    asd
    dsa

--- .cz-config.js
+++ .cz-config.js
@@ -1,16 +1,16 @@
 'use strict';
 module.exports = {
 	types: [
-		{ value: '✨feat', name: '新增:    新的内容' },
-		{ value: '🐛fix', name: '修复:    修复一个Bug' },
-		{ value: '📝docs', name: '文档:    变更的只有文档' },
-		{ value: '💄style', name: '格式:    空格, 分号等格式修复' },
-		{ value: '♻️refactor', name: '重构:    代码重构，注意和特性、修复区分开' },
-		{ value: '⚡️perf', name: '性能:    提升性能' },
-		{ value: '✅test', name: '测试:    添加一个测试' },
-		{ value: '🔧chore', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-		{ value: '⏪revert', name: '回滚:    代码回退' },
-		{ value: 'build', name: '回滚:    代码回退' },
+		{ value: 'feat', name: '新增:    新的内容' },
+		{ value: 'fix', name: '修复:    修复一个Bug' },
+		{ value: 'docs', name: '文档:    变更的只有文档' },
+		{ value: 'style', name: '格式:    空格, 分号等格式修复' },
+		{ value: 'refactor', name: '重构:    代码重构，注意和特性、修复区分开' },
+		{ value: 'perf', name: '性能:    提升性能' },
+		{ value: 'test', name: '测试:    添加一个测试' },
+		{ value: 'chore', name: '工具:    开发工具变动(构建、脚手架工具等)' },
+		{ value: 'revert', name: '回滚:    代码回退' },
+		{ value: 'build', name: '打包:    代码打包' },
 	],
 	scopes: [
 		{ name: 'leetcode' },
@@ -34,12 +34,14 @@ module.exports = {
 		scope: '选择一个scope (可选):',
 		// used if allowCustomScopes is true
 		customScope: 'Denote the SCOPE of this change:',
-		subject: '短说明:\n',
+		subject: '短说明:（必填）\n',
 		body: '长说明，使用"|"换行(可选)：\n',
 		breaking: '非兼容性说明 (可选):\n',
-		footer: '关联关闭的issue，例如：#31, #34(可选):\n',
+		footer: '请输入要关闭的issue，例如：#31, #34(可选):\n',
 		confirmCommit: '确定提交说明?(yes/no)',
 	},
+	// 跳过问题
+	// skipQuestions: ['body', 'footer'],
 	allowCustomScopes: true,
 	allowBreakingChanges: ['特性', '修复'],
 	// limit subject length
